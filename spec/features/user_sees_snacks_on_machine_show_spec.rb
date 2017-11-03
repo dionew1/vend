40518dd6c10c2e3b4c5e94a515920f3e1d19a00a
Dione Wilson <dionew1@gmail.com>
2017-11-03 10:34:46 -0600

    Price and name showing on vending show page

--- spec/features/user_sees_snacks_on_machine_show_spec.rb
+++ spec/features/user_sees_snacks_on_machine_show_spec.rb
@@ -4,10 +4,29 @@ feature "User visits a specific vending machine page" do
   scenario "user sees name of all the snacks in machine with price" do
     owner = Owner.create(name: "Sal")
     machine = Machine.create(location: "School", owner: owner)
+    snack1 = Snack.create(name: "Jerky", price: 5)
+    snack2 = Snack.create(name: "Peanuts", price: 3)
+    MachineSnack.create(machine: machine, snack: snack1)
+    MachineSnack.create(machine: machine, snack: snack2)
 
     visit machine_path(machine)
 
-    expect(page).to have_content("Jerky")
-    expect(page).to have_content("$4.00")
+    expect(page).to have_content(snack1.name)
+    expect(page).to have_content(snack1.price)
+    expect(page).to have_content(snack2.name)
+    expect(page).to have_content(snack2.price)
+  end
+
+  scenario "user sees a an average price for all of the snacks" do
+    owner = Owner.create(name: "Sal")
+    machine = Machine.create(location: "School", owner: owner)
+    snack1 = Snack.create(name: "Jerky", price: 5)
+    snack2 = Snack.create(name: "Peanuts", price: 3)
+    MachineSnack.create(machine: machine, snack: snack1)
+    MachineSnack.create(machine: machine, snack: snack2)
+
+    visit machine_path(machine)
+
+    expect(page).to have_content("Average Price: ")
   end
 end
